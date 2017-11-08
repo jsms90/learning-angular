79a6a2cf09010cbd727be5962c6b870cae828ea1
Nicholas Johnson <nicholas@beamery.com>
2017-11-08 12:04:27 +0000

    Demo

--- intern_onboarding/003_ng2/exercise_2/demo/cat-app/src/app/app.component.ts
+++ intern_onboarding/003_ng2/exercise_2/demo/cat-app/src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Cats are Cool!!';
+  isDisabled = false;
+  txt = 'cats';
+  count = 0;
+  x = 0
+  y = 0
+
+  constructor() {
+  	setInterval(() => {
+  		this.txt += " cats"
+  	}, 5000);
+  }
+
+  handleMousemove(evt) {
+  	this.x = evt.pageX;
+  	this.y = evt.pageY;
+  }
 }
