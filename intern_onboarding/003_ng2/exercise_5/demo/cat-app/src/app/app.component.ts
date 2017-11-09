1519517d86ea7e2161c97e32480e006df3492f8e
Nicholas Johnson <nicholas@beamery.com>
2017-11-09 16:10:05 +0000

    Exercise

--- intern_onboarding/003_ng2/exercise_5/demo/cat-app/src/app/app.component.ts
+++ intern_onboarding/003_ng2/exercise_5/demo/cat-app/src/app/app.component.ts
@@ -6,6 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+	attacker:any
+	runAway:any
+
 	tabby = {
 		name:"tabby",
 		color:'orange',
--- intern_onboarding/003_ng2/exercise_5/demo/cat-app/src/app/cat/cat.component.ts
+++ intern_onboarding/003_ng2/exercise_5/demo/cat-app/src/app/cat/cat.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-cat',
@@ -8,6 +8,6 @@ import { Component, Input } from '@angular/core';
 export class CatComponent {
 
   @Input() cat = {}
-  @Input() toastLevel = 0
-
+  @Output() attack = new EventEmitter()
+  @Output() run = new EventEmitter()
 }
